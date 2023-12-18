110499a994b53819966e5a5346cc1bfd3b883811
YashRajput233 <yashp7813@gmail.com>
2023-12-18 16:44:25 +0530

    changes 2

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-import express from "express";
-import "./src/config/db.js";
-import userRouter from "./src/routes/userRoute.js";
+import express from 'express'
+import './src/config/db.js'
+import userRouter from './src/routes/userRoute.js'
 const app = express();
-const port = 5000;
+const port=5000;
 app.use(express.json());
-app.use("/api/v1", userRouter);
+app.use('/api/v1',userRouter)
+
 
 app.get('/',function(req,res){
   res.send('welcome')
 })
 
-app.listen(port, () => {
-  console.log(`server is running on ${port} ..👍`);
-  const error = false;
-  if (error) {
-    console.log(`error running is server`, error);
-  }
-});
+app.listen(port,()=>{
+    console.log(`server is running on ${port} ..👍`);
+    const error=false
+    if(error){
+        console.log(`error running is server`,error)
+    }
+})
